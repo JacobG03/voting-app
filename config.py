5e0a460a7aab5a0bea0d41f5e67df7ff0b6a81a3
JacobG03 <jacobgluszek03@gmail.com>
2021-11-07 12:58:49 +0100

    minor dev fixes

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
   # General
   SECRET_KEY = os.environ.get('SECRET_KEY') or '$@ul-tra-safe-secret-key$@'
-
   # Database config
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_IRL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -21,6 +20,6 @@ class Config(object):
   JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=1800)
   JWT_COOKIE_SECURE = False
   JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=15)
-  JWT_COOKIE_CSRF_PROTECT = True 
+  JWT_COOKIE_CSRF_PROTECT = False   # Change to True on Production build 
   JWT_ACCESS_CSRF_HEADER_NAME = "X-CSRF-TOKEN-ACCESS"
   JWT_REFRESH_CSRF_HEADER_NAME = "X-CSRF-TOKEN-REFRESH"
