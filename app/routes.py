1f0a3645529f3d2eb1b73f7cb0c8db50d0779cd7
JacobG03 <jacobgluszek03@gmail.com>
2021-11-07 11:05:10 +0100

    register added

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,53 @@
-from app import app
-from flask import jsonify
+from app import app, db
+from app.models import User
+from flask import jsonify, request
+from app.schemas import CreateRegisterSchema
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+registerSchema = CreateRegisterSchema()
 
 
 @app.get('/api')
 def api():
   return jsonify({
     'message': 'It appears that the API should technically maybe work. :)'
+  }), 200
+
+
+@app.post('/api/register')
+def register():
+  # receives data
+  data = request.get_json(silent=True)
+  # validate data
+  errors = registerSchema.validate(data)
+  if errors:
+    return jsonify({
+      'errors': errors
+    }), 422
+
+  # checks whether passwords are identical
+  elif data['password'] != data['password2']:
+    return jsonify({
+      'errors': {
+        'password': ['Passwords must match'],
+        'password2': ['Passwords must match']
+      }
+    }), 422
+
+  # hash password
+  hashed_password = generate_password_hash(data['password'], method='sha256')
+
+  user = User(
+    username=data['username'],
+    email=data['email'],
+    password=hashed_password
+  )
+  db.session.add(user)
+  db.session.commit()
+
+  return jsonify({
+    'message': 'User created successfully.'
   }), 200
 
 
