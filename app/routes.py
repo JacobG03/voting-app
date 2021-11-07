d86cd79f536552f22d3c1604a1da0b54527acbf4
JacobG03 <jacobgluszek03@gmail.com>
2021-11-07 10:30:27 +0100

    api working

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,9 @@
 from app import app
+from flask import jsonify
+
+
+@app.get('/api')
+def api():
+  return jsonify({
+    'message': 'It appears that the API should technically maybe work. :)'
+  }), 200
