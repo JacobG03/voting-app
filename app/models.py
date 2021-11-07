64d317e41193fc195f582d2f5692a01295bd6da0
JacobG03 <jacobgluszek03@gmail.com>
2021-11-07 10:01:23 +0100

    poll & children added

--- app/models.py
+++ app/models.py
@@ -6,8 +6,38 @@ class User(db.Model):
   username = db.Column(db.String(64), unique=True, nullable=False)
   email = db.Column(db.String(128), unique=True, nullable=False)    # In this case doesn't have to be an actual email
   password = db.Column(db.String(256), nullable=False)
+  votes = db.relationship('Vote', backref=db.backref('user', lazy=True))
 
   def __repr__(self):
     return f'{self.username}'
 
 
+class Poll(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  topic = db.Column(db.String(128), nullable=False)
+  options = db.relationship('Option', backref=db.backref('poll', lazy=True))
+  votes = db.relationship('Vote', backref=db.backref('poll', lazy=True))
+
+  def __repr__(self):
+    return f'{self.topic}'
+
+
+class Option(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'), nullable=False)
+  body = db.Column(db.String(128), nullable=False)
+  votes = db.relationship('Vote', backref=db.backref('option', lazy=True))
+
+  def __repr__(self):
+    return f'{self.body}'
+
+
+class Vote(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'), nullable=False)
+  option_id = db.Column(db.Integer, db.ForeignKey('option.id'), nullable=False)
+
+  def __repr__(self):
+    return f'{Option.query.get(self.id).body}'
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from app import app, db
-from app.models import User
+from app.models import User, Poll, Option, Vote
 
 
 @app.shell_context_processor
 def make_shell_context():
   return {
     'db': db,
-    'User': User
+    'User': User,
+    'Poll': Poll,
+    'Option': Option,
+    'Vote': Vote
   }
 
 
